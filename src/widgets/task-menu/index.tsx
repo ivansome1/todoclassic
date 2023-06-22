dad0c3aea8b6924d2983c28889d2b51dca69cb25
Ivan Nazarov <ivansome108@gmail.com>
2023-06-22 11:45:52 +0300

    Add task menu

---
+++ src/widgets/task-menu/index.tsx
@@ -0,0 +1,35 @@
+import { RemoveTaskMenuItem } from "@/features/remove-task";
+import { Icon } from "@/shared/ui";
+import { IconButton, Menu } from "@mui/material";
+import { FC, useState } from "react";
+
+interface TaskMenuProps {
+  id: number;
+}
+
+export const TaskMenu: FC<TaskMenuProps> = ({ id }) => {
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+  const open = Boolean(anchorEl);
+
+  return (
+    <>
+      <IconButton
+        onClick={(event) => {
+          setAnchorEl(event.currentTarget);
+        }}
+      >
+        <Icon>more_vert</Icon>
+      </IconButton>
+
+      <Menu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={() => {
+          setAnchorEl(null);
+        }}
+      >
+        <RemoveTaskMenuItem id={id} />
+      </Menu>
+    </>
+  );
+};
