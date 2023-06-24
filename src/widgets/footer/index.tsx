cba961a9c0221f6ec8edb71c70338001a094ff08
Ivan Nazarov <119253797+ivansome1@users.noreply.github.com>
2023-06-24 20:06:06 +0300

    Made footer transparent

--- src/widgets/footer/index.tsx
+++ src/widgets/footer/index.tsx
@@ -1,14 +1,12 @@
-import { Link, Paper, Typography } from "@mui/material";
+import { Link, Typography } from "@mui/material";
 
 export const Footer = () => {
   return (
-    <Paper sx={{ borderRadius: 0 }}>
       <Typography variant="body2" sx={{ textAlign: "center", py: 1 }}>
         2023, see source code on{" "}
         <Link href="https://github.com/ivansome1/todoclassic">
           github.com/ivansome1/todoclassic
         </Link>
       </Typography>
-    </Paper>
   );
 };
