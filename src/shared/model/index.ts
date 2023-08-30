2571a730f737d62cddbfca4ceb2b01449fa8f80b
Ivan Nazarov <ivansome108@gmail.com>
2023-08-30 11:37:54 +0300

    Add theme-color support

--- src/shared/model/index.ts
+++ src/shared/model/index.ts
@@ -1,4 +1,17 @@
+import { useEffect, useState, Dispatch, SetStateAction } from "react";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 export const useAppDispatch: () => RootDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useThemeColor = (): [string, Dispatch<SetStateAction<string>>] => {
+  const [themeColor, setThemeColor] = useState<string>("#191919");
+
+  useEffect(() => {
+    document
+      .querySelector('meta[name="theme-color"]')
+      ?.setAttribute("content", themeColor);
+  }, [themeColor]);
+
+  return [themeColor, setThemeColor];
+};
